879c3c8a81957865446158fb30288bf458fd86ed
Madhu Sowmy <109299565+MadhuSowmya-24@users.noreply.github.com>
2022-12-06 10:58:13 +0530

    Add files via upload

---
+++ dec6-1.js
@@ -0,0 +1,6 @@
+function check(a,b){
+    sum=a+b;
+    if(a==10||b==10||sum==10) return true ;
+    }
+   console.log(check(10,1));
+   console.log(check(5,5));
---
+++ dec6-2.js
@@ -0,0 +1,8 @@
+function shiftToLeft(a,b){
+    let x;
+    x= a* Math.pow(2,b);
+    return x;
+}
+console.log(shiftToLeft(5,2));
+console.log(shiftToLeft(10,3));
+console.log(shiftToLeft(-32,2));
